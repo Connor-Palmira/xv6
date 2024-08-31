abe68eb8de38ab4f4118bc42ce777ae60122c065
Connor Palmira <palmic1@unlv.nevada.edu>
2024-08-31 16:18:39 -0700

    my solution for hello xv6

---
+++ user/hello.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main()
+{
+    printf("Hello world, this is Connor Palmira.\n");
+    printf("I am in CS 370, Fall 2024, section 1001.\n\n");
+
+    exit(0);
+}
